876139d97cd7406297bc763037d73aa92855f9f9
SnowsFE <snoerkr@gmail.com>
2024-05-08 08:32:09 +0900

    TypeScript Study

---
+++ ts/src/app/components/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "styled-components";
+const comehome: React.FC = () => {
+  return (
+    <Come>
+      <p>안녕하세요</p>
+    </Come>
+  );
+};
+
+const Come = styled.div`
+  p {
+    color: blue;
+  }
+`;
+export default comehome;
---
+++ ts/src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <Come>
+      <p>안녕하세요</p>
+    </Come>
+  );
+}
+
+const Come = styled.div`
+  p {
+    color: red;
+  }
+`;
+export default Home;
